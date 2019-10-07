837aee31f8cd7d602ee0336ee02a8e60b33f55d3
RavenSuzumiya <axel_dmh@hotmail.com>
2019-10-07 12:07:39 -0500

    correct format on stock import

--- mtgStock.php
+++ mtgStock.php
@@ -75,83 +75,79 @@ $title = "MTG Stock Import"
             ?>
 
             <form autocomplete="off" action="./stock_events.php"  method="post">
-                <div class="autocomplete" style="width:300px;">
-                    <input type="hidden" name="action" value="add"/>
-                    <table>
-                        <tr><td>Quantity:</td><td><input type="number" name="quantity" value="1"/></td></tr>
-                        <tr><td>Name:</td><td><input id="card" type="text" name="card" placeholder="card" onchange="updPrintings()"/></td></tr>
-                        <tr><td>Set:</td><td><select id="printings" name="printings" style="width:200px"/></td>                   </tr> 
-                        <tr><td>Location:</td><td><input id="location" type="text" name="location" placeholder="location"/></td></tr>
-                        <tr><td>Foil:</td><td><input id="foil" type="checkbox" name="foil"></td></tr>
-                    </table>
-                </div>
+                <input type="hidden" name="action" value="add"/>
+                <table>
+                    <tr><td>Quantity:</td><td><input type="number" name="quantity" value="1"/></td></tr>
+                    <tr><td>Name:</td><td><div class="autocomplete" style="width:300px;"><input id="card" type="text" name="card" placeholder="card" onchange="updPrintings()"/></div></td></tr>
+                    <tr><td>Set:</td><td><select id="printings" name="printings" style="width:200px"/></td></tr> 
+                    <tr><td>Location:</td><td><input id="location" type="text" name="location" placeholder="location"/></td></tr>
+                    <tr><td>Language:</td><td><select id="language" name="language" style="width:200px"></select></td></tr>
+                    <tr><td>Notes:</td><td><input id="notes" type="text" name="notes" placeholder="notes"/></td></tr>
+                    <tr><td>Foil:</td><td><input id="foil" type="checkbox" name="foil"></td></tr>
+                </table>
                 <input type="submit">
             </form>
-
         </div>
 
         <script src="./includes/autocomplete.js"></script>
         <script>
-                        var cards = [<?php
+            var cards = [<?php
             $cards = find([], $db, 'cards', ['projection' => ['name' => 1]]);
             foreach ($cards as $card) {
                 echo '"' . str_replace('"', '\\"', $card['name']) . '", ';
             }
             ?>];
-                        var sets = {<?php
+            var sets = {<?php
             $sets = find([], $db, 'sets');
             foreach ($sets as $set) {
                 echo "\"$set[code]\":\"$set[name]\",";
             }
             ?>};
 
-                        autocomplete(document.getElementById("card"), cards);
+            autocomplete(document.getElementById("card"), cards);
 
-                        function updPrintings() {
-                            //apparently the autocomplete takes time so we use a timeout
-                            setTimeout(function () {
+            function updPrintings() {
+                //apparently the autocomplete takes time so we use a timeout
+                setTimeout(function () {
 
-                                var FD = new FormData();
-                                var XHR = new XMLHttpRequest();
+                    var FD = new FormData();
+                    var XHR = new XMLHttpRequest();
 
-                                FD.append('action', 'printings');
-                                FD.append('card', document.getElementById('card').value);
+                    FD.append('action', 'printings');
+                    FD.append('card', document.getElementById('card').value);
 
-                                XHR.onreadystatechange = function () {
-                                    if (XHR.readyState === 4 && XHR.status === 200) {
-                                        clearOptions();
-                                        populatePrintings(JSON.parse(XHR.responseText));
-                                    }
-                                };
-
-                                var url = "./stock_events.php";
-                                XHR.open("POST", url, true);
-                                XHR.send(FD);
-                            }, 150);
-
+                    XHR.onreadystatechange = function () {
+                        if (XHR.readyState === 4 && XHR.status === 200) {
+                            clearOptions();
+                            populatePrintings(JSON.parse(XHR.responseText));
                         }
+                    };
 
-                        function clearOptions() {
-                            printingsObj = document.getElementById('printings');
+                    var url = "./stock_events.php";
+                    XHR.open("POST", url, true);
+                    XHR.send(FD);
+                }, 150);
 
-                            for (i = printingsObj.options.length - 1; i >= 0; i--) {
-                                printingsObj.remove(i);
-                            }
-                        }
+            }
 
-                        function populatePrintings(values) {
-                            var sel = document.getElementById('printings');
+            function clearOptions() {
+                printingsObj = document.getElementById('printings');
 
-                            for (i = 0; i < values.length; i++) {
-                                var opt = document.createElement('option');
-                                opt.appendChild(document.createTextNode(sets[values[i]]));
-                                opt.value = values[i];
-                                sel.appendChild(opt);
-                            }
-                        }
+                for (i = printingsObj.options.length - 1; i >= 0; i--) {
+                    printingsObj.remove(i);
+                }
+            }
 
+            function populatePrintings(values) {
+                var sel = document.getElementById('printings');
+
+                for (i = 0; i < values.length; i++) {
+                    var opt = document.createElement('option');
+                    opt.appendChild(document.createTextNode(sets[values[i]]));
+                    opt.value = values[i];
+                    sel.appendChild(opt);
+                }
+            }
         </script>
-
     </body>
-
 </html>
